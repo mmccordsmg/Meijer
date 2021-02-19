05464f5e4491314cadcec5ad8967d8889b4a875b
mmccordsmg <mmccord@skymailnj.com>
2021-02-19 17:19:26 -0500

    f-ed the fstring

--- clip_meijer_coupon.py
+++ clip_meijer_coupon.py
@@ -12,7 +12,7 @@ init_meijer_connection_user(args.username, args.password)
 
 result = clip_meijer_coupon(args.offerid)
 if not result == True:
-	print ("Error clipping coupon {args.offerid} - {result}")
+	print (f"Error clipping coupon {args.offerid} - {result}")
 	exit()
 else:
 	print(f"Clipped offer {args.offerid} successfully on account {args.username}.")
