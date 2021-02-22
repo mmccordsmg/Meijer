bf63e4a681a7ea97f0405b95546f59c56edb2548
mmccordsmg <mmccord@skymailnj.com>
2021-02-22 16:19:43 -0500

    Adds expiration date to log

--- bruteforce_meijer_coupons.py
+++ bruteforce_meijer_coupons.py
@@ -26,13 +26,13 @@ if earnedoffers:
 	for offer in sorted(earnedoffers, key = lambda i: i['meijerOfferId']):
 		if offer.get("meijerOfferId") in validids:
 			print(f'{offer.get("meijerOfferId")} - reward - {offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}')
-			results.append({'meijerOfferId': offerid, 'status': 'valid reward', 'description': f'{offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}'})
+			results.append({'meijerOfferId': offerid, 'status': 'valid reward', 'description': f'{offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}', 'expirationDate': offer.get("expirationDate")})
 coupons = get_all_clipped_meijer_coupons()
 if coupons:
 	for coupon in sorted(coupons, key = lambda i: i['meijerOfferId']):
 		if coupon.get("meijerOfferId") in validids:
 			print(f'{coupon.get("meijerOfferId")} - coupon - {coupon.get("title").strip()} {coupon.get("description").strip()}')
-			results.append({'meijerOfferId': offerid, 'status': 'valid coupon', 'description': f'{coupon.get("title").strip()} {coupon.get("description").strip()}'})
+			results.append({'meijerOfferId': offerid, 'status': 'valid coupon', 'description': f'{coupon.get("title").strip()} {coupon.get("description").strip()}', 'expirationDate': coupon.get("redemptionEndDate")})
 			tounclip.append(coupon.get("meijerOfferId"))
 
 with open(f'{args.username}_{args.prefix}.txt', 'w') as outfile:
