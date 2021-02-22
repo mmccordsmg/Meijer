4c8a0c10c9a59871bdbce1c3a9d8a7deb6edc3cb
mmccordsmg <mmccord@skymailnj.com>
2021-02-22 16:48:14 -0500

    Fixes ids showing as 9999 in log.  Fixes missing InProgress and Available rewards in log.

--- bruteforce_meijer_coupons.py
+++ bruteforce_meijer_coupons.py
@@ -1,6 +1,6 @@
 from meijer_funcs import clip_meijer_coupon, init_meijer_connection_user, unclip_meijer_coupon, get_all_clipped_meijer_coupons, get_meijer_offers, strip_tags
 import argparse
-from tqdm import trange
+from tqdm import trange, tqdm
 import json
 
 parser = argparse.ArgumentParser(description="Finds  Meijer coupons for account specified")
@@ -21,22 +21,26 @@ for offerid in trange(int(f'{args.prefix}0000'), int(f'{args.prefix}9999')+1):
 		validids.append(offerid)
 	else:
 		results.append({'meijerOfferId': offerid, 'status': result})
-earnedoffers = get_meijer_offers('Earned')
-if earnedoffers:
-	for offer in sorted(earnedoffers, key = lambda i: i['meijerOfferId']):
-		if offer.get("meijerOfferId") in validids:
-			print(f'{offer.get("meijerOfferId")} - reward - {offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}')
-			results.append({'meijerOfferId': offerid, 'status': 'valid reward', 'description': f'{offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}', 'expirationDate': offer.get("expirationDate")})
+
+for offertype in ['Earned', 'InProgress', 'Available']:
+	offers = get_meijer_offers(offertype)
+	if offers:
+		for offer in sorted(offers, key = lambda i: i['meijerOfferId']):
+			if offer.get("meijerOfferId") in validids:
+				print(f'{offer.get("meijerOfferId")} - {offertype} reward - {offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}')
+				results.append({'meijerOfferId': offer.get("meijerOfferId"), 'status': f'{offertype} reward', 'description': f'{offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}', 'expirationDate': offer.get("expirationDate")})
+
 coupons = get_all_clipped_meijer_coupons()
 if coupons:
 	for coupon in sorted(coupons, key = lambda i: i['meijerOfferId']):
 		if coupon.get("meijerOfferId") in validids:
 			print(f'{coupon.get("meijerOfferId")} - coupon - {coupon.get("title").strip()} {coupon.get("description").strip()}')
-			results.append({'meijerOfferId': offerid, 'status': 'valid coupon', 'description': f'{coupon.get("title").strip()} {coupon.get("description").strip()}', 'expirationDate': coupon.get("redemptionEndDate")})
+			results.append({'meijerOfferId': coupon.get("meijerOfferId"), 'status': 'Valid coupon', 'description': f'{coupon.get("title").strip()} {coupon.get("description").strip()}', 'expirationDate': coupon.get("redemptionEndDate")})
 			tounclip.append(coupon.get("meijerOfferId"))
 
 with open(f'{args.username}_{args.prefix}.txt', 'w') as outfile:
 	json.dump(sorted(results, key = lambda i: i['meijerOfferId']), outfile, indent=2)
 
-for offerid in tounclip:
+print('Unclipping please wait')
+for offerid in tqdm(tounclip):
 	unclip_meijer_coupon(offerid)
