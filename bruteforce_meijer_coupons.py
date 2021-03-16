9d087a6b9b68f3dc8c81ddd5ed3f028d5afe7044
mmccordsmg <mmccord@skymailnj.com>
2021-03-16 15:46:35 -0400

    Updates bruteforcer for 100k ranges

--- bruteforce_meijer_coupons.py
+++ bruteforce_meijer_coupons.py
@@ -2,22 +2,48 @@ from meijer_funcs import clip_meijer_coupon, init_meijer_connection_user, unclip
 import argparse
 from tqdm import trange, tqdm
 import json
+import random
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Finds  Meijer coupons for account specified")
 parser.add_argument("-u",	"--username",		help="Username (Email address)",								required=True,	type=str)
 parser.add_argument("-p",	"--password",		help="Password",																required=True,	type=str)
-parser.add_argument("-r",	"--prefix",			help="prefix (6 digits)",												required=True,	type=str)
+parser.add_argument("-r",	"--prefix",			help="prefix (5 digits)",												required=True,	type=str)
 
 args = parser.parse_args()
 
 init_meijer_connection_user(args.username, args.password)
-
-startnum = int(f'{args.prefix}0000')
-endnum = int(f'{args.prefix}9999')+1
+if len(args.prefix) != 5:
+	print('Wrong prefix length')
+	exit()
+	
+startnum = int(f'{args.prefix}00000')
+endnum = int(f'{args.prefix}99999')+1
 results = []
-tounclip = []
-for offerid in trange(startnum, endnum):
+
+checkrange = []
+
+outname = f'{args.username}_{args.prefix}_{datetime.now().strftime("%Y-%m-%d-%H_%M_%S")}.txt'
+
+for x in range(startnum, endnum):
+	checkrange.append(x)
+
+def process_coupons():
+	global results, checkrange
+	coupons = get_all_clipped_meijer_coupons()
+	if coupons:
+		for coupon in sorted(coupons, key = lambda i: i['meijerOfferId']):
+			if coupon.get("meijerOfferId") in checkrange:
+				#print(f'{coupon.get("meijerOfferId")} - coupon - {coupon.get("title").strip()} {coupon.get("description").strip()}')
+				results.append({'meijerOfferId': coupon.get("meijerOfferId"), 'status': 'Valid coupon', 'description': f'{coupon.get("title").strip()} {coupon.get("description").strip()}', 'expirationDate': coupon.get("redemptionEndDate")})
+				unclip_meijer_coupon(coupon.get("meijerOfferId"))
+		
+for offerid in tqdm(checkrange):
+	if offerid%1000 == 0:
+		process_coupons()
 	result = clip_meijer_coupon(offerid)
+	#if result == 'Success':
+		#print(f'{offerid}: Success!')
 	if (not result == True) and (not result == 'Offer is already clipped by this shopper.'):
 		results.append({'meijerOfferId': offerid, 'status': result})
 
@@ -25,21 +51,11 @@ for offertype in ['Earned', 'InProgress', 'Available']:
 	offers = get_meijer_offers(offertype)
 	if offers:
 		for offer in sorted(offers, key = lambda i: i['meijerOfferId']):
-			if offer.get("meijerOfferId") in range(startnum, endnum):
-				print(f'{offer.get("meijerOfferId")} - {offertype} reward - {offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}')
+			if offer.get("meijerOfferId") in checkrange:
+				#print(f'{offer.get("meijerOfferId")} - {offertype} reward - {offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}')
 				results.append({'meijerOfferId': offer.get("meijerOfferId"), 'status': f'{offertype} reward', 'description': f'{offer.get("title").strip()} {strip_tags(offer.get("description")).strip()}', 'expirationDate': offer.get("expirationDate")})
 
-coupons = get_all_clipped_meijer_coupons()
-if coupons:
-	for coupon in sorted(coupons, key = lambda i: i['meijerOfferId']):
-		if coupon.get("meijerOfferId") in range(startnum, endnum):
-			print(f'{coupon.get("meijerOfferId")} - coupon - {coupon.get("title").strip()} {coupon.get("description").strip()}')
-			results.append({'meijerOfferId': coupon.get("meijerOfferId"), 'status': 'Valid coupon', 'description': f'{coupon.get("title").strip()} {coupon.get("description").strip()}', 'expirationDate': coupon.get("redemptionEndDate")})
-			tounclip.append(coupon.get("meijerOfferId"))
+process_coupons()
 
-with open(f'{args.username}_{args.prefix}.txt', 'w') as outfile:
-	json.dump(sorted(results, key = lambda i: i['meijerOfferId']), outfile, indent=2)
-
-print('Unclipping please wait')
-for offerid in tqdm(tounclip):
-	unclip_meijer_coupon(offerid)
+with open(outname, 'w') as outfile:
+	json.dump(sorted(results, key = lambda i: i['status']), outfile, indent=2)
